804ca1e5de7c536b78352885c93d993aaaade80c
Mobin.vr <Mobin.vr@outlook.com>
2024-09-11 23:56:03 +0330

    initial leaflet implementing

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,31 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './Map.module.css';
 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+
 function Map() {
    const navigate = useNavigate();
+   const [mapPosition, setMapPosition] = useState([40, 0]);
 
    return (
       <div className={styles.mapContainer} onClick={() => navigate('form')}>
-         Map
+         <MapContainer
+            center={mapPosition}
+            zoom={13}
+            scrollWheelZoom={true}
+            className={styles.map}
+         >
+            <TileLayer
+               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+               url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+            />
+            <Marker position={mapPosition}>
+               <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+               </Popup>
+            </Marker>
+         </MapContainer>
       </div>
    );
 }
