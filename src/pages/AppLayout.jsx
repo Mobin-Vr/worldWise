ebb2f090736fd09cdab3ae1bdc9951a563e5e450
Mobin.vr <Mobin.vr@outlook.com>
2024-09-14 14:33:10 +0330

    Add authentication flow with login and logout functionality

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,23 @@ import Sidebar from '../components/Sidebar';
 import Map from '../components/Map';
 
 import styles from './AppLayout.module.css';
+import { useAuth } from '../contexts/FakeAuthContext';
+import User from '../components/User';
 
 function AppLayout() {
+   const { isAuthenticated } = useAuth();
+
    return (
       <div className={styles.app}>
-         <Sidebar />
-         <Map />
+         {isAuthenticated ? (
+            <>
+               <Sidebar />
+               <Map />
+               <User />
+            </>
+         ) : (
+            ''
+         )}
       </div>
    );
 }
